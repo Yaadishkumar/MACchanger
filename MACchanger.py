e7b0359a99df1a5a38666848d1db66f07ba9d9bf
Yaadishkumar <yaadish1617@gmail.com>
2026-03-29 12:37:42 +0530

    update 1

--- MACchanger.py
+++ MACchanger.py
@@ -3,9 +3,10 @@ import optparse
 
 parser=optparse.OptionParser()
 parser.add_option("-i","--interface",dest="interface",help="help to change its MAC address")
+parser.add_option("-m","--new_mac",dest="new_mac",help="enter new mac address")
 parser.parse_args()
 
-subprocess.run("ifconfig")
+
 
 interface=input("interface>")
 new_mac=input("new_mac>")
@@ -13,4 +14,3 @@ new_mac=input("new_mac>")
 subprocess.run(["ifconfig",interface,"down"])
 subprocess.run(["ifconfig",interface,"hw","ether",new_mac])
 subprocess.run(["ifconfig",interface,"up"])
-subprocess.run("ifconfig")
