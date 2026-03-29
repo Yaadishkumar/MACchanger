cbef6b00df1854dab4dadb0aa30cbadd23d327b7
Yaadishkumar <yaadish1617@gmail.com>
2026-03-29 12:58:40 +0530

    add MAC address changer script for Linux

--- MACchanger.py
+++ MACchanger.py
@@ -4,12 +4,12 @@ import optparse
 parser=optparse.OptionParser()
 parser.add_option("-i","--interface",dest="interface",help="help to change its MAC address")
 parser.add_option("-m","--new_mac",dest="new_mac",help="enter new mac address")
-parser.parse_args()
+(options,argument) parser.parse_args()
 
 
 
-interface=input("interface>")
-new_mac=input("new_mac>")
+interface=options.interface
+new_mac=options.new_mac
 
 subprocess.run(["ifconfig",interface,"down"])
 subprocess.run(["ifconfig",interface,"hw","ether",new_mac])
